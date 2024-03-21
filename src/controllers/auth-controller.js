78f384c98f25fb9092a6bd0f7daff0ba904b3c22
Guigzouz <benard.guillaume0@gmail.com>
2024-03-21 11:35:46 +0100

    issuing jwt on logging in

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,20 +1,29 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
 
 const createUser = async (req, res) => {
   try {
     const user = {
-      firstname: req.body.firstName,
-      lastname: req.body.lastName,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
       hashedPassword: await bcrypt.hash(req.body.password, 10),
       phoneNumber: req.body.phoneNumber,
-      countryKey: req.body.country,
+      countryKey: req.body.countryKey,
     };
 
-    console.log(user);
+    const existingEmail = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (existingEmail) {
+      res.status(400).send("this email is already used");
+      return;
+    }
 
     const createdUser = await User.create(user);
+
     console.log("created user:", createdUser);
     res.status(201).send(createdUser);
   } catch {
@@ -23,19 +32,27 @@ const createUser = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const user = users.find((user) => (user.name = req.body.name));
+  const user = await User.findOne({ where: { email: req.body.email } });
   if (user == null) {
     return res.status(400).send("Cannot find user");
   }
 
   try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.send("Success");
+    if (await bcrypt.compare(req.body.password, user.hashedPassword)) {
+      console.log("success");
+
+      const token = jwt.sign(
+        { _id: user.id, email: user.email },
+        process.env.JWT_SECRET
+      );
+
+      console.log("token :", token);
+      res.status(200).send({ authData: user, jwt: token });
     } else {
-      res.send("Not Allowed");
+      res.status(203).send("Not Allowed");
     }
   } catch {
-    res.status(500).send();
+    res.status(500).send("Internal server error");
   }
 };
 

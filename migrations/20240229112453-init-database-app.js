a8b1c6c7d2a99830c4c139a0ebf792f47adc5afb
Guigzouz <benard.guillaume0@gmail.com>
2025-01-09 10:58:19 +0100

    migrations needed fixing

--- migrations/20240229112453-init-database-app.js
+++ migrations/20240229112453-init-database-app.js
@@ -3,7 +3,13 @@ const app = require("../models/index");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
+    await app.FileAttachment.sync();
     await app.User.sync();
     await app.Token.sync();
+  },
+
+  async down(queryInterface, Sequelize) {
+    await app.User.drop();
+    await app.Token.drop();
   },
 };

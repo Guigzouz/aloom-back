67c7088bb7c65b2eb340de9cbeac0dba525348b1
Guigzouz <benard.guillaume0@gmail.com>
2025-01-09 11:56:17 +0100

    trying ci/cd #1

--- migrations/20240523132157-add-few-socials-tables.js
+++ migrations/20240523132157-add-few-socials-tables.js
@@ -7,4 +7,10 @@ module.exports = {
     await app.Group.sync();
     await app.UserGroup.sync();
   },
+
+  async down(queryInterface, Sequelize) {
+    await app.UserGroup.drop();
+    await app.Group.drop();
+    await app.Friend.drop();
+  },
 };
